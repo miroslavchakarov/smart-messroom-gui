6a5bc18d155f7c5f359c66235f9768d5bd44113f
miroslavchakarov <miroslav.chakarov@outlook.com>
2022-04-18 10:05:56 +0300

    ver 1.0-gui

---
+++ README.md
@@ -0,0 +1,35 @@
+
+This is an example of the 2004 LCD screen and HX711 ADC up and running on Rust and Raspberry Pi.
+
+More about the system:
+
+This is a system called Smart Messroom, built in 2018 on Raspberry Pi and Windows IoT Core. It is now being rebuilt and will soon be able to run on Rust for a high level of protection and speed. 
+The system is not a weight scale! It measures food all the time. After a certain amount of food has been lifted from the food container, it starts calculating the difference in weight between NOW and the time the weight started to differ (food to be taken).
+It also contains a 7-inch LCD screen.
+
+See more on the video below:
+
+https://1drv.ms/v/s!Ak5sft2RFM38jb4jOWfsavIP3ROe2A?e=IfUv9y
+
+(Note: Give it a few seconds to load as onedrive is a bit slower.)
+
+
+As per this moment it can show the load cell ADC data on the 2004 LCD screen. (It will get more complex in time.)
+
+Update: It can now show not only raw values but also the values converted in grams and kilograms!
+
+Update 14.04.: It can now show the quantity of food taken from the weight scale.
+
+The near-future goals for the Smart Messroom are (TODO):
+
+ - (DONE!) To make the ADC (Analogue-to-Digital-Converter) and the 2004 LCD screen work with Rust and RPI2. (See other two repos)
+ - (Almost done!) Raise an event when worker picks up food from the food container and assign the difference in grams to the current customer.
+ - Add new customer and display the customer number, product and quantity of food for the current client on the LCD 2004 screen.
+ - Ability to reset and start measurement from the beginning in case quantity is not appropriate.
+
+
+The long-term goals for this system are the following:
+ - Calibration funcion (tara) while program running. (Currently the calibration value is hard coded)
+ - (DONE!) Having a touch-friendly GUI on the 7-inch Touchscreen for the worker to operate with the device. (Currently testing various GUI libraries  suitable for Raspberry Pi)
+ - Having a payment system integrated, eventually with cryptocurrency or fiat currency.
+
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,38 @@
 use fltk::{enums::{Align, Color, Font, FrameType}, prelude::*, *, };
 
+use std::thread;
+use linux_embedded_hal::Pin;
+use linux_embedded_hal::sysfs_gpio::Direction;
+use hd44780_hal::HD44780;
+
+//hx711
+use rppal::{spi::{Spi, Bus, SlaveSelect, Mode, Error},hal::Delay};
+use hx711_spi::Hx711;
+use nb::block;
+
+const one_kg_value: f32 = 130670.0;
+const N: f32 = 30.0;
+const ReadLoopCount: u8 = 5;
+
+
+
 const BLUE: Color = Color::from_hex(0x42A5F5);
 const SEL_BLUE: Color = Color::from_hex(0x2196F3);
 const GRAY: Color = Color::from_hex(0x757575);
 const GREEN: Color = Color::from_hex(0x9CC28F);
-const WIDTH: i32 = 600;
-const HEIGHT: i32 = 400;
+const WIDTH: i32 = 800;
+const HEIGHT: i32 = 500;
 
-fn main() {
+struct AdcData{
+    adcRawVal: f32,
+    adcVal : f32,
+    zeroVal: f32,
+    taraVal: f32,
+    kgVal: f32,
+    previousKgVal: f32,
+}
+
+fn main() -> Result<(), Error> {
     let app = app::App::default();
     let mut win = window::Window::default()
         .with_size(WIDTH, HEIGHT)
@@ -61,9 +86,145 @@ fn main() {
     // End theming
 
     but.set_callback(move |_| {
-        let label = (weight_lbl.label().parse::<i32>().unwrap() + 1).to_string();
-        weight_lbl.set_label(&label);
+        //let label = (weight_lbl.label().parse::<i32>().unwrap() + 1).to_string();
+       // weight_lbl.set_label(&label);
     });
 
     app.run().unwrap();
+    //done with GUI inits
+
+
+    let mut ADC = AdcData{
+        adcRawVal: 0.0,
+        adcVal : 0.0,
+        zeroVal: 0.0,
+        taraVal: 0.0,
+        kgVal: 0.0,
+        previousKgVal: 0.0,
+    };
+
+    //hx711 declarations
+    let spi = Spi::new(Bus::Spi0, SlaveSelect::Ss0, 1_000_000, Mode::Mode0)?;
+    let mut hx711 = Hx711::new(spi, Delay::new());
+    
+    //init the screen
+	hx711.reset()?;
+
+    //calibration (tara)
+    for i in 0..N as i32 {
+        let reading = block!(hx711.read()).unwrap() as f32;
+        println!("{:>2}: {}", i, reading);
+        ADC.zeroVal += reading;
+    }
+    ADC.zeroVal /= N; //tara
+    println!("Tara: {}", ADC.zeroVal);
+
+    //screen declarations
+    let rs = Pin::new(13);
+    let en = Pin::new(19);
+
+    let db4 = Pin::new(26);
+    let db5 = Pin::new(16);
+    let db6 = Pin::new(20);
+    let db7 = Pin::new(21);
+
+    rs.export().unwrap();
+    en.export().unwrap();
+    
+    db4.export().unwrap();
+    db5.export().unwrap();
+    db6.export().unwrap();
+    db7.export().unwrap();
+
+    rs.set_direction(Direction::Low).unwrap();
+    en.set_direction(Direction::Low).unwrap();
+    
+    db4.set_direction(Direction::Low).unwrap();
+    db5.set_direction(Direction::Low).unwrap();
+    db6.set_direction(Direction::Low).unwrap();
+    db7.set_direction(Direction::Low).unwrap();
+
+    //4-bit communication with display
+    let mut lcd = HD44780::new_4bit(
+        rs,
+        en,
+    
+        db4,
+        db5,
+        db6,
+        db7,
+        linux_embedded_hal::Delay,
+    );
+    
+    lcd.reset();
+    lcd.clear();
+    lcd.set_display_mode(true, false, false);
+    lcd.write_str("Customer nr. 4");
+
+    lcd.set_cursor_pos(40);
+    lcd.write_str("WELCOME!");
+    lcd.set_cursor_pos(30);
+    lcd.write_str("--- g.");
+
+    
+    let mut counter: u8 = 0;
+    let mut flag: bool = false;
+    loop {
+        ADC.previousKgVal = ADC.kgVal;
+        ADC.adcVal = 0.0;
+        
+        for _ in 0..ReadLoopCount {
+            ADC.adcRawVal = block!(hx711.read()).unwrap() as f32;
+            ADC.adcVal += ADC.adcRawVal;
+        }
+        ADC.adcVal /= ReadLoopCount as f32;
+        ADC.taraVal = ADC.adcVal-ADC.zeroVal;
+        ADC.kgVal = ADC.taraVal/one_kg_value;
+        println!(
+            "Read: {} --- Tara val: {} --- kg: {:.3}", 
+            ADC.adcVal as i32, 
+            ADC.taraVal as i32, 
+            ADC.kgVal);
+                
+        if (ADC.kgVal - ADC.previousKgVal) > 0.002 {
+            println!("--- START LISTENING --- {}", ADC.kgVal - ADC.previousKgVal);
+            lcd.set_cursor_pos(40);
+            lcd.write_str("Calculating...");
+            calc_status.set_label("Calculating...");
+        }
+
+        if (ADC.previousKgVal - ADC.kgVal) > 0.002 {
+            println!("---STOP LISTENING--- To be added: {:.3}", ADC.kgVal);
+            lcd.set_cursor_pos(40);
+            lcd.write_str("Almost there...");
+            calc_status.set_label("Almost there...");
+
+            flag = true;
+            counter = 0;
+        }
+        
+        if flag == true {
+            counter += 1;
+            lcd.set_cursor_pos(30);
+            let s = format!("{:.0} g.   ", (ADC.kgVal * 1000.0).abs());
+            lcd.write_str(&s);
+            weight_lbl.set_label(&s);
+            if counter == 5{
+                println!("{:.3} added to customer", ADC.kgVal);
+                
+                lcd.set_cursor_pos(40);
+                lcd.write_str("Green salad:   ");
+                flag = false;
+                counter = 0;
+            }
+        }
+        
+    }
+
+    
+
+    
+
+
+
 }
